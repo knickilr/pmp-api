b414e7f80bda1a4652ed62e05e8ce8e0a494d1df
knickilr <knickilr@gmail.com>
2022-03-19 15:49:45 +0530

    feat(deploy): adding procfile for deployment

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,7 @@ import projectRoute from './routes/project'
 import employeeRoute from './routes/employee'
 
 const app = express();
+const port = process.env.Port || 8989
 mongoose.connect('mongodb+srv://nikhil:nikhil@cluster0.mftgu.mongodb.net/pmp')
 mongoose.Promise = global.Promise;
 
@@ -12,6 +13,6 @@ app.use(express.json())
 app.use("/api/project", projectRoute)
 app.use("/api/employee", employeeRoute)
 
-app.listen(8989, ()=>{
-    console.log('Server started......')
+app.listen(port, ()=>{
+    console.log('Server started......', port)
 })
